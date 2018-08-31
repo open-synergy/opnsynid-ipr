846aea0d2fb2af03ee96cfa2bae2fc28832b7e81
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-31 14:36:27 +0700

    [8.0.1.0.0][ipr_pos_accurate_info]

---
+++ ipr_pos_accurate_info/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Accurate Information for PoS",
+    "version": "8.0.1.0.0",
+    "category": "Point Of Sale",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "point_of_sale"
+    ],
+    "data": [
+        "views/stock_warehouse_view.xml",
+        "views/res_company_view.xml"
+    ]
+}
