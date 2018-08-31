32c53ceeb9f2ef2b054f0dd8cc73b0cd1c204b94
mikevhe18 <viriyananda.michael@gmail.com>
2018-08-31 14:36:27 +0700

    [8.0.1.0.0][ipr_pos_accurate_info]

---
+++ ipr_pos_accurate_info/models/res_company.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = 'res.company'
+
+    accurate_pos_ar_account = fields.Char(
+        string='PoS AR Account'
+        )
---
+++ ipr_pos_accurate_info/models/stock_warehouse.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class StockWarehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    accurate_dept_id = fields.Char(
+        string="DEPTID"
+    )
+    accurate_warehouse_id = fields.Char(
+        string="WAREHOUSEID"
+    )
+    accurate_customer_id = fields.Char(
+        string="CUSTOMERID"
+    )
