8a61ece0c99d34d7f01ddbe79257aa259269054c
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-07 10:31:51 +0700

    [8.0.1.0.0][ipr_stock_compliment_list]
    Adding product_code on report
    
    Revisi Report

---
+++ ipr_stock_compliment_list/reports/stock_move_compliment_list.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class StockMoveComplimentList(models.Model):
+    _name = "stock.move_compliment_list"
+    _inherit = "stock.move_list_common"
+    _auto = False
+
+    def _get_subtype(self, cr):
+        query = """
+            SELECT  res_id
+            FROM    ir_model_data
+            WHERE   module = 'stock_promotion_operation'
+            AND     name = 'customer_promotion_subtype'
+        """
+        cr.execute(query)
+        subtype_id = cr.fetchone()[0]
+        return subtype_id
