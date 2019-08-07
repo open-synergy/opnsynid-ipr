b790e986d6d3f4d3a3916b6b56e34ea73cc7343e
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-07 10:33:41 +0700

    [8.0.1.2.1][ipr_stock_list_common]
    Adding product_code
    
    Revisi

--- ipr_stock_list_common/reports/stock_move_list_common.py
+++ ipr_stock_list_common/reports/stock_move_list_common.py
@@ -34,6 +34,9 @@ class StockMoveListCommon(models.AbstractModel):
         string="Internal Reference",
         comodel_name="stock.picking",
     )
+    product_code = fields.Char(
+        string="Product Code"
+    )
     product_id = fields.Many2one(
         string="Product",
         comodel_name="product.product",
@@ -57,6 +60,15 @@ class StockMoveListCommon(models.AbstractModel):
         string="Picking Type",
         comodel_name="stock.picking.type",
     )
+    price_unit = fields.Float(
+        string="Price",
+        required=True,
+        digits=(16,2),
+    )
+    create_uid = fields.Many2one(
+        string="User",
+        comodel_name="res.users",
+    )
 
     def _get_subtype(self, cr):
         return False
@@ -76,17 +88,27 @@ class StockMoveListCommon(models.AbstractModel):
     def _select_field(self):
         select_field_str = """
                 A.id as move_id,
-                A.warehouse_id AS warehouse_id,
+                C.warehouse_id AS warehouse_id,
                 A.company_id AS company_id,
                 A.date AS date,
                 B.partner_id AS partner_id,
                 A.picking_id AS picking_id,
+                x.default_code AS product_code,
                 A.product_id AS product_id,
                 A.product_qty AS product_qty,
                 A.location_id AS source_loc_id,
                 A.location_dest_id AS dest_loc_id,
                 y.uom_id AS product_uom_id,
-                B.picking_type_id AS picking_type_id
+                B.picking_type_id AS picking_type_id,
+                A.create_uid AS create_uid,
+        	    CASE
+            		WHEN
+            		    Z.price_unit is NULL
+            		THEN
+            		    0.0
+        	    ELSE
+        		      Z.price_unit
+        	    END AS price_unit
         """
         return select_field_str
 
@@ -104,6 +126,39 @@ class StockMoveListCommon(models.AbstractModel):
             JOIN product_template AS y ON x.product_tmpl_id = y.id
         """
         return join_str
+
+    def _sub_join(self):
+        sub_join_str = """
+            LEFT JOIN (
+            	SELECT  A2.move_id,
+            		SUM(
+            		    CASE
+            		        WHEN
+            			    A3.value_text='real'
+            		        THEN
+            			    A1.cost * A1.qty
+            		    ELSE
+            		        A4.standard_price * A1.qty
+            		    END
+            		) AS price_unit
+            	FROM 	stock_quant AS A1
+            	JOIN	stock_quant_move_rel AS A2 ON A2.quant_id=A1.id
+            	JOIN	(
+            		SELECT 	value_text,
+            			CAST(split_part(res_id, ',', 2) AS INTEGER) AS product_id
+            		FROM 	ir_property
+            		WHERE	name='cost_method'
+            	) AS A3 ON A3.product_id=A1.product_id
+            	JOIN	(
+            		SELECT 	value_float AS standard_price,
+            			CAST(split_part(res_id, ',', 2) AS INTEGER) AS product_id
+            		FROM 	ir_property
+            		WHERE	name='standard_price'
+            	) AS A4 ON A4.product_id=A1.product_id
+            	GROUP BY A2.move_id
+            ) AS Z ON A.id=Z.move_id
+        """
+        return sub_join_str
 
     def _where(self, subtype_id):
         where_str = """
@@ -131,11 +186,13 @@ class StockMoveListCommon(models.AbstractModel):
             %s
             %s
             %s
+            %s
         )""" % (
             self._table,
             self._select(self._order_by()),
             self._from(),
             self._join(),
+            self._sub_join(),
             self._where(subtype_id),
             self._order_by(),
         ))
