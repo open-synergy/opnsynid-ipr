7677a53cc147dd0149f966ec9193be5dd0379a21
Andhitia Rama <andhitia.r@gmail.com>
2019-08-09 06:58:33 +0700

    Revert "[8.0.1.0.0][ipr_stock_transfer_in_list]"
    This reverts commit 8b0558b9a06f4ae8ab71aa5e8192fe0881791c5f.

--- ipr_stock_transfer_in_list/reports/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2019 OpenSynergy Indonesia
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from . import (
-    stock_move_transfer_in_list,
-    stock_transfer_in_list,
-)
