337de12c9ba35331eeec6f2531e6dcc621773646
Andhitia Rama <andhitia.r@gmail.com>
2019-08-09 06:58:33 +0700

    Revert "[8.0.1.2.1][ipr_stock_list_common]"
    This reverts commit 7aeb4217768245063307460f4ef80452579b9d86.

--- ipr_stock_list_common/__openerp__.py
+++ ipr_stock_list_common/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "IPR - Stock List Common",
-    "version": "8.0.1.3.0",
+    "version": "8.0.1.2.0",
     "category": "Stock",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
